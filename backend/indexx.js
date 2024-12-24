53cd6ffe7415e36068c2cff2f47cd9d4a4af7126
shubechavan <142329297+shubechavan@users.noreply.github.com>
2024-12-24 11:06:38 +0530

    Update indexx.js

--- backend/indexx.js
+++ backend/indexx.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { createtodo, updatetodo } = require('./types'); // Ensure these are properly defined
-const todos = require('./db'); // Import your database model
+const { createtodo, updatetodo } = require('./types'); 
+const todos = require('./db'); 
 const {MONGODB_URI} = require('/todoapp/config');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-// POST: Create a new todo
 app.post("/todo", async function (req, res) {
     const createPayload = req.body;
     const parsedPayload = createtodo.safeParse(createPayload);
@@ -33,7 +31,6 @@ app.post("/todo", async function (req, res) {
     res.json({ success: "todo created" });
 });
 
-// GET: Fetch all todos
 app.get("/todos", async function (req, res) {
     console.log("Fetching your data");
     const allTodos = await todos.find({});
@@ -42,7 +39,6 @@ app.get("/todos", async function (req, res) {
     });
 });
 
-// PUT: Mark todo as completed
 app.put("/todocompleted", async function (req, res) {
     const updatePayload = req.body;
     const parsedPayload = updatetodo.safeParse(updatePayload);
@@ -60,7 +56,6 @@ app.put("/todocompleted", async function (req, res) {
     res.json({ success: "todo completed" });
 });
 
-// Start the server
 app.listen(3000, () => {
     console.log("Listening on http://localhost:3000");
 });
