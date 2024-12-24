68ce4747b6447435dd3d8cf249aedddf2d040d3d
shubechavan <142329297+shubechavan@users.noreply.github.com>
2024-12-24 11:09:09 +0530

    Update Todos.jsx

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -2,8 +2,8 @@ export function Todos() {
 return <div>
 <h1>go to gym </h1>
 <h2>
-    go to gym you nigger   </h2>
-<button>mark as completed nigger</button>
+    go to gym you huzz   </h2>
+<button>mark as completed huzz</button>
 
 
 
