fa173727b0f87abb8f55e77d465f1ceea763ef15
shubechavan <142329297+shubechavan@users.noreply.github.com>
2024-12-24 11:07:39 +0530

    Update types.js

--- backend/types.js
+++ backend/types.js
@@ -1,15 +1,13 @@
 const zod = require("zod");
 
-// Schema for creating a todo
 const createtodo = zod.object({
     title: zod.string().nonempty("Title is required"),
     description: zod.string().nonempty("Description is required"),
 });
 
-// Schema for updating a todo
 const updatetodo = zod.object({
     id: zod.string().nonempty("Todo ID is required"),
-    completed: zod.boolean().optional(), // Optional `completed` field
+    completed: zod.boolean().optional(), 
 });
 
 module.exports = {
